683ca29003eb560621967df2a1ba1eca272f7a6e
Vishal Verma <vishal.verma_cs18@gla.ac.in>
2020-01-19 00:56:41 +0530

    output generated

--- src/com/company/Evod.java
+++ src/com/company/Evod.java
@@ -6,6 +6,7 @@ public class Evod {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        System.out.println(a % 2 == 0 ? "Even" : "Odd");
 
     }
 }
